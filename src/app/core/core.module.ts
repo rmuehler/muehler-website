3d075ee61dddec6ae6011e7a7076c02220fd218d
rmuehler <robertmuehler@gmail.com>
2023-10-12 22:12:54 -0400

    uninstall ngx-bootstrap

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    HttpClientModule
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    HttpClientModule
   ]
 })
 export class CoreModule { }
