47f83fa9688c610e73d987eee3d31e5a40f57fba
rmuehler <robertmuehler@gmail.com>
2023-10-12 22:21:54 -0400

    update core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    RouterModule,
-    NgbModule,
-    HttpClientModule
-  ],
-  exports: [
     CommonModule,
     RouterModule,
     NgbModule,
