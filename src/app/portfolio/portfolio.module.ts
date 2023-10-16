9b6bb6f66e5e35ce02535313f3599bf1825f97b0
rmuehler <robertmuehler@gmail.com>
2023-10-16 16:12:57 -0400

    add: about and portofolio components

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent, ProjectsComponent],
   imports: [CommonModule, PortfolioRoutingModule],
 })
 export class PortfolioModule {}
